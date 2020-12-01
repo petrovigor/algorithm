10b8352330548cb9eef70399315b461e2d75cb95
Alexey.Ismailov <wc3e@list.ru>
2020-12-01 17:47:19 +0300

    added parenthess processing; algorithm adds whitespaces after each number, so you can easely parse it lately

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <stack>
+#include <cctype>
 
 #include <cassert> //tests only
 
@@ -8,34 +9,67 @@
 #endif //_DEBUG
 
 //returns reverse polish notation of source string
-//doesn't check consistency expression passed
-//doesn't process brackets & unary operators
+//doesn't process unary operators
 std::string ReversePolishNotation(const std::string& src) {
   std::string dst;
   std::stack<char> q;
 
+  int n = 0;
+  bool flg = 0;
+
   for(const auto& c : src) {
-    if(std::isdigit(c)) {
-      dst += c;
-    } else {
-      auto getOperatorPriority = [](const char lhs) -> int {
-        switch(lhs) {
-          case '*':
-          case '/':
-            return 2;
-          case '+':
-          case '-':
-            return 1;
-        }
-        return 0;
-      };
+    if(std::isspace(c))
+      continue;
 
-      while(!q.empty() && getOperatorPriority(q.top()) >= getOperatorPriority(c)) {
-        dst += q.top();
-        q.pop();
+    if(c == '(') {
+      if(flg) {
+        dst += std::to_string(n) + ' ';
+        flg = 0;
+        n = 0;
       }
-
       q.push(c);
+    } else if(std::isdigit(c)) {
+      if(!flg) {
+        flg = 1;
+      }
+      n *= 10;
+      n += static_cast<int>(c - 48);
+    } else {
+      if(flg) {
+        dst += std::to_string(n) + ' ';
+        flg = 0;
+        n = 0;
+      }
+      if(c == ')') {
+        while(!q.empty() && q.top() != '(') {
+          dst += q.top();
+          q.pop();
+        }
+
+        if(q.top() == '(') {
+          q.pop();
+        }
+      } else {
+        auto getOperatorPriority = [](const char lhs) -> int {
+          switch(lhs) {
+            case '*':
+            case '/':
+              return 2;
+            case '+':
+            case '-':
+              return 1;
+          }
+          return 0; // '('
+        };
+
+        while(!q.empty() && getOperatorPriority(q.top()) >= getOperatorPriority(c)) {
+          if(q.top() != '(')
+            dst += q.top();
+          q.pop();
+        }
+
+        q.push(c);
+      }
     }
 
 #if _DEBUG
@@ -50,9 +84,12 @@ std::string ReversePolishNotation(const std::string& src) {
     std::cout << std::endl;
 #endif //_DEBUG
   }
-
+  if(flg) {
+    dst += std::to_string(n) + ' ';
+  }
   while(!q.empty()) {
-    dst += q.top();
+    if(q.top() != '(')
+      dst += q.top();
     q.pop();
   }
 
@@ -64,12 +101,11 @@ std::string ReversePolishNotation(const std::string& src) {
 }
 
 int main() {
-
   assert(ReversePolishNotation("") == "");
-  assert(ReversePolishNotation("2+3*5+9") == "235*+9+");
-  assert(ReversePolishNotation("2*9+9+9/8/7/6/5*1*3") == "29*9+98/7/6/5/1*3*+");
-  assert(ReversePolishNotation("1+2+3+4+5") == "12+3+4+5+");
-  assert(ReversePolishNotation("1+2-3+4-5") == "12+3-4+5-");
+  assert(ReversePolishNotation("2+3*5+9") == "2 3 5 *+9 +");
+  assert(ReversePolishNotation("2*9+9+9/8/7/6/5*1*3") == "2 9 *9 +9 8 /7 /6 /5 /1 *3 *+");
+  assert(ReversePolishNotation("1+2+3+4+5") == "1 2 +3 +4 +5 +");
+  assert(ReversePolishNotation("1+2-3+4-5") == "1 2 +3 -4 +5 -");
 
   return 0;
 }

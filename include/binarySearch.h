ad3b122a081b20bd9aeb264036bb6931bee33db3
Igor Petrov <igor.petrov.pm@gmail.com>
2020-12-02 06:50:05 +0300

    added desc to binarySearch.h fn

--- include/binarySearch.h
+++ include/binarySearch.h
@@ -2,4 +2,8 @@
 
 #include <vector>
 
+//search for a number 'target' in the array 'arr'
+//arr should be sorted before passed into the fn
+//searches with O(log n) complexity
+//returns false if number is not found, returns true instead
 bool binarySearch(const std::vector<int>& arr, const int target);
